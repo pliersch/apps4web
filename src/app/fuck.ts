a7bcefec411e9f284108f67ed291900af77c9404
pliersch <hourby@gmail.com>
2021-11-04 14:58:35 +0100

    - fucking clean project

---
+++ src/app/fuck.ts
@@ -0,0 +1,14 @@
+export class Fuck {
+
+  public title: string;
+  public imageUrl: string;
+  public rows: number;
+  public cols: number;
+
+  constructor(title: string, imageUrl:string, rows:number, cols:number) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.rows = rows;
+    this.cols = cols;
+  }
+}
