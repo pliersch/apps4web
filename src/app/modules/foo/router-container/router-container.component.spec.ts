b24c20cda1d24a30f36fe7f5da13ad0727e25aaf
pliersch <hourby@gmail.com>
2021-11-14 11:36:31 +0100

    - move files into new project

---
+++ src/app/modules/foo/router-container/router-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RouterContainerComponent } from './router-container.component';
+
+describe('RouterContainerComponent', () => {
+  let component: RouterContainerComponent;
+  let fixture: ComponentFixture<RouterContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RouterContainerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RouterContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
