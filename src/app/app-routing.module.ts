3622d52f6d71ca5e2663a3b1e8732444bcd26b43
pliersch <hourby@gmail.com>
2021-11-14 17:03:43 +0100

    - add recipes module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {LegalNoticeComponent} from "@modules/legal-notice/legal-notice.component
 
 const accountModule = () => import('@app/modules/account/account.module').then((x) => x.AccountModule);
 const adminModule = () => import('@app/modules/admin/admin.module').then((x) => x.AdminModule);
+const recipesModule = () => import('@app/modules/recipes/recipes.module').then((x) => x.RecipesModule);
 const fooModule = () => import('@app/modules/foo/foo.module').then((x) => x.FooModule);
 const profileModule = () => import('@app/modules/profile/profile.module').then((x) => x.ProfileModule);
 const galleryModule = () => import('@app/modules/gallery/gallery.module').then((x) => x.GalleryModule);
@@ -19,6 +20,7 @@ const routes: Routes = [
       { path: 'gallery', loadChildren: galleryModule },
       { path: 'foo', loadChildren: fooModule },
       { path: 'admin', loadChildren: adminModule },
+      { path: 'recipes', loadChildren: recipesModule },
       { path: 'profile', loadChildren: profileModule },
     ]}
 ];
