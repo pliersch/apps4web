4169c9176e2a4dab37144e4fcc7c2596db164824
pliersch <hourby@gmail.com>
2022-02-06 17:32:32 +0100

    - beginning image control

---
+++ src/app/modules/gallery/components/share/image-control/image-control.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-image-control',
+  templateUrl: './image-control.component.html',
+  styleUrls: ['./image-control.component.scss']
+})
+export class ImageControlComponent {
+
+  constructor() {
+  }
+
+}
