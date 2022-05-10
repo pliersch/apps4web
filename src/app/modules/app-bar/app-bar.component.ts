2107f09f206f32a625e00732d45c26e05bf15619
pliersch <hourby@gmail.com>
2022-05-10 19:36:43 +0200

    - not happy

--- src/app/modules/app-bar/app-bar.component.ts
+++ src/app/modules/app-bar/app-bar.component.ts
@@ -1,16 +1,43 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
+import {trigger, transition, state, animate, style} from '@angular/animations';
 
 @Component({
   selector: 'app-appbar',
   templateUrl: './app-bar.component.html',
-  styleUrls: ['./app-bar.component.scss']
+  styleUrls: ['./app-bar.component.scss'],
+  animations: [
+    trigger('childAnimation', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1.0,
+        border: 1.0
+      })),
+      state('closed', style({
+        height: '200px',
+        opacity: 0,
+        border: 0
+      })),
+      transition('* => *', [
+        animate('0.1s')
+      ]),
+    ]),
+  ],
 })
-export class AppBarComponent {
+export class AppBarComponent implements OnInit {
+  @ViewChild('background') background: ElementRef;
+  @ViewChild('menuBtn') menuBtn: ElementRef;
   @Input() appName: string | undefined;
   @Output() toggleNavEvent = new EventEmitter<string>();
   @Output() switchThemeEvent = new EventEmitter<string>();
 
-  constructor() {
+  isOpen = false;
+
+  toggle(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor(private renderer: Renderer2) {
   }
 
   emitNavToggle(): void {
@@ -19,5 +46,19 @@ export class AppBarComponent {
 
   onSwitchTheme($event: string): void {
     this.switchThemeEvent.emit($event);
+  }
+
+  ngOnInit(): void {
+    this.renderer.listen('window', 'click', (e: PointerEvent) => {
+      const element = this.background.nativeElement;
+      const isMouseBelow = e.clientY > element.offsetTop + element.offsetHeight;
+      if (e.target !== this.background.nativeElement && isMouseBelow) {
+        this.isOpen = false;
+      }
+    });
+  }
+
+  onClickGoogle(): void {
+    console.log('AppBarComponent onClickGoogle: ',)
   }
 }
