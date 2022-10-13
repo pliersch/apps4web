7a50dbf7ec08294c496ed9934cb3402519766c74
pliersch <hourby@gmail.com>
2022-10-13 06:19:02 +0200

    - use dialog for delete photos

--- src/app/modules/gallery/components/explorer/delete-photo-dialog/gallery-delete-photo.component.ts
+++ src/app/modules/gallery/components/explorer/delete-photo-dialog/gallery-delete-photo.component.ts
@@ -1,20 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { DeletePhotoDialogData, EditTagsDialogData } from "@gallery/components/explorer/gallery-explorer.component";
+import { DeletePhotoDialogData } from "@gallery/components/explorer/gallery-explorer.component";
 
 @Component({
   selector: 'app-gallery-delete-photo',
   templateUrl: './gallery-delete-photo.component.html',
   styleUrls: ['./gallery-delete-photo.component.scss']
 })
-export class GalleryDeletePhotoComponent implements OnInit {
+export class GalleryDeletePhotoComponent {
 
   constructor(public dialogRef: MatDialogRef<GalleryDeletePhotoComponent>,
               @Inject(MAT_DIALOG_DATA) public data: DeletePhotoDialogData) { }
-
-  ngOnInit(): void {
-    console.log('GalleryDeletePhotoComponent ngOnInit: ', this.data.photo)
-  }
 
   onSave(): void {
     this.dialogRef.close(true);
