c9c8a05521cabc980e0383814841f52f10946927
Patrick Liersch <hourby@gmail.com>
2023-04-13 19:33:01 +0200

    - dashboard wip

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { BreakpointService } from "@app/common/services/breakpoint.service";
 import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,6 +13,11 @@ export class DashboardComponent implements OnInit {
 
   protected readonly Breakpoints = Breakpoints;
   currentBreakpoint: Observable<string>;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
 
   constructor(private breakpointObserver: BreakpointObserver,
               private breakpointService: BreakpointService) { }
