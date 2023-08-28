d546de88ab59163cae64b832fc17bc47d886647f
Patrick Liersch <hourby@gmail.com>
2023-08-28 20:40:45 +0200

    - delete "dynamic load module" code

--- src/app/core/components/app-bar/app-bar.component.ts
+++ src/app/core/components/app-bar/app-bar.component.ts
@@ -1,12 +1,22 @@
+import { SigninComponent } from '@account/components/signin-menu/signin.component';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, isDevMode, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Route, RouterState } from "@app/core/stores/routes/router.state";
+import { ThemeMenuComponent } from '@modules/themes/menus/theme-menu.component';
 import { Select } from "@ngxs/store";
 import { Observable } from "rxjs";
+import { WidgetComponent } from '../widget/widget.component';
 
 @Component({
   selector: 'app-appbar',
   templateUrl: './app-bar.component.html',
-  styleUrls: ['./app-bar.component.scss']
+  styleUrls: ['./app-bar.component.scss'],
+  standalone: true,
+  imports: [NgIf, MatToolbarModule, MatButtonModule, MatIconModule, RouterLink, WidgetComponent, ThemeMenuComponent, SigninComponent, NgFor, RouterLinkActive, AsyncPipe]
 })
 export class AppBarComponent {
 
@@ -18,12 +28,6 @@ export class AppBarComponent {
   @Select(RouterState.getAccessibleRoutes)
   routes$: Observable<Route[]>;
   protected readonly isDevMode = isDevMode;
-// @ViewChild("authMenuPlaceHolder", {read: ViewContainerRef})
-  // authMenuPlaceHolder!: ViewContainerRef;
-
-  // routes$ = this.routeService.getRoutes();
-
-  // constructor(/*private injector: Injector*/ ) { }
 
   emitNavToggle(): void {
     this.toggleNavEvent.emit('toggle');
@@ -32,19 +36,5 @@ export class AppBarComponent {
   onSwitchTheme($event: string): void {
     this.switchThemeEvent.emit($event);
   }
-
-  // ngAfterViewInit(): void {
-  //   void this.loadFeature()
-  // }
-
-  // async loadFeature(): Promise<void> {
-  //   const {AuthModule} = await import('../auth/auth.module');
-  //   createNgModuleRef(AuthModule, this.injector);
-  //
-  //   const {instance} = this.authMenuPlaceHolder.createComponent(AuthMenuComponent);
-  //   // manually call ngOnChanges for dynamically created components
-  //   instance.ngOnChanges(); // necessary?
-  // }
-
 
 }
