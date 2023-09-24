cbefa52a86c0b59cd4cda6a340bc83f1f759de0f
Patrick Liersch <hourby@gmail.com>
2023-09-24 13:06:42 +0200

    - wip site-builder

---
+++ src/app/modules/playground/components/browser/component-browser.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-component-browser',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './component-browser.component.html',
+  styleUrls: ['./component-browser.component.scss']
+})
+export class ComponentBrowserComponent {
+
+}
