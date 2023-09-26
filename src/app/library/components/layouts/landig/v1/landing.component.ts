aca1a992abbe9530c36ea336e6c829fde551a0e9
Patrick Liersch <hourby@gmail.com>
2023-09-26 21:35:47 +0200

    - wip site-builder

--- src/app/library/components/layouts/landig/v1/landing.component.ts
+++ src/app/library/components/layouts/landig/v1/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ScrollSpyDirective } from "@app/common/directives/scroll-spy.directive";
+import { WindowScrollSpyDirective } from "@app/common/directives/window-scroll-spy.directive";
 
 @Component({
   selector: 'app-landing',
@@ -7,13 +8,14 @@ import { ScrollSpyDirective } from "@app/common/directives/scroll-spy.directive"
   styleUrls: ['./landing.component.scss'],
   standalone: true,
   imports: [
-    ScrollSpyDirective
+    ScrollSpyDirective,
+    WindowScrollSpyDirective
   ]
 })
 
 export class LandingComponent {
 
   testScrollSpy(event: string): void {
-    // console.log('LandingComponent switchControlbarDesign: ', event)
+    console.log('LandingComponent switchControlbarDesign: ', event)
   }
 }
