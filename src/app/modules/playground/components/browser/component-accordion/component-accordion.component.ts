eaa5eb7b0d7d634a16b30a8523f5963d3d79f6af
Patrick Liersch <hourby@gmail.com>
2023-09-28 18:11:27 +0200

    - component accordion

--- src/app/modules/playground/components/browser/component-accordion/component-accordion.component.ts
+++ src/app/modules/playground/components/browser/component-accordion/component-accordion.component.ts
@@ -1,15 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatExpansionModule } from "@angular/material/expansion";
+import { MatListModule } from "@angular/material/list";
+
+export interface AssetCategory {
+  name: string;
+  items: string[];
+}
 
 @Component({
   selector: 'app-component-accordion',
   standalone: true,
-  imports: [CommonModule, MatExpansionModule],
+  imports: [CommonModule, MatExpansionModule, MatListModule],
   templateUrl: './component-accordion.component.html',
   styleUrls: ['./component-accordion.component.scss']
 })
 export class ComponentAccordionComponent {
 
-  panelOpenState = false;
+  assetCategories: AssetCategory[] =
+    [
+      {name: 'appbar', items: ['Foo1', 'Foo2']},
+      {name: 'landing', items: ['Bar1', 'Bar2']},
+      {name: 'contact', items: ['Baz1', 'Baz2']},
+    ];
+
+  onSelectAsset(item: string): void {
+    console.log('ComponentAccordionComponent onSelectAsset: ', item)
+  }
 }
